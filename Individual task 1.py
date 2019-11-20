ba55919303b6630bd49108cea47dd4d46b125765
will8889 <williamluciantos@gmail.com>
2019-11-20 19:07:24 +0700

    update

--- Individual task 1.py
+++ Individual task 1.py
@@ -1,38 +1,38 @@
+#1
 Degrees = 150
-Radians = Degrees * 3.14 / 180
-print(Degrees)
-print(Radians)
+rumus = 3.14 / 180
 
-student1 = 80.0
-student2 = 90.0
-student3 = 66.5
-average = (student1 + student2 + student3) / 3
-print(average)
+def radians():
+    print(Degrees * rumus)
 
-class1_total = 32
-class2_total = 45
-class3_total  = 51
-class1 = class1_total // 5
-class2 = class2_total // 7
-class3 = class3_total // 6
-print(class1)
-print(class2)
-print(class3)
-class1 = class1_total % 5
-class2 = class2_total % 7
-class3 = class3_total % 6
-print(class1)
-print(class2)
-print(class3)
+radians()
 
+#2
+total = 80.0 + 90.0 + 66.5
+def average():
+    print(total / 3)
+    
+average()
+
+#3
+
+
+
+#4
 pi = 3.14
 pie_diameter = 55.4
 pie_radius = pie_diameter / 2
 circumference = 2 * pi * pie_radius
-circumference_msg = "Jimmy’s pie has a circumference:"
-print(circumference_msg, circumference)
+circumference_msg = "jimmy's pie has a circumference"
+def end():
+    print(circumference_msg, circumference)
 
-velocity = 343
-frequency = 256
-wavelength = velocity / frequency
-print(wavelength)
+end()
+
+#5
+v = 343
+f = 256
+def w(v,f):
+    print(v / f)
+
+w(v,f)
